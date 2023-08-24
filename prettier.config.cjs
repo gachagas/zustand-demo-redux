39a5c73e3aa6e1df9fb021c6fefb1c4988a0d099
Gerlito Chagas <gerlito.chagas@gmail.com>
2023-08-24 19:05:13 +0800

    prettier auto sort imports

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,6 +1,27 @@
-/** @type {import("prettier").Config} */
-const config = {
-  plugins: [require.resolve("prettier-plugin-tailwindcss")],
+/** @type {import("prettier").Options} */
+module.exports = {
+  plugins: [
+    require.resolve("@trivago/prettier-plugin-sort-imports"),
+    require.resolve("prettier-plugin-tailwindcss"),
+  ],
+  importOrder: [
+    "^next$",
+    "^next",
+    "^react$",
+    "^react",
+    "<THIRD_PARTY_MODULES>",
+    "^@/(.*)",
+    "^[./]",
+  ],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
+  arrowParens: "avoid",
+  bracketSpacing: true,
+  endOfLine: "lf",
+  jsxSingleQuote: false,
+  quoteProps: "consistent",
+  semi: true,
+  singleQuote: false,
+  tabWidth: 2,
+  trailingComma: "all",
 };
-
-module.exports = config;
