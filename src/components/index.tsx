1b2369919761ade7b9a53e2bda643007e77d3b34
Gerlito Chagas <gerlito.chagas@gmail.com>
2023-08-24 20:55:10 +0800

    remove redux components

--- src/components/index.tsx
+++
@@ -1,6 +0,0 @@
-import AddToFirstButton from "./AddToFirstButton";
-import AddToSecondButton from "./AddToSecondButton";
-import FirstValue from "./FirstValue";
-import SecondValue from "./SecondValue";
-
-export { AddToFirstButton, AddToSecondButton, FirstValue, SecondValue };
